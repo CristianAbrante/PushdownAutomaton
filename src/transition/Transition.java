fb0203a4fb3d60c7065c89254e881c036d2614a0
Cristian Abrante <alu0100945850@ull.edu.es>
2018-10-21 15:16:25 +0100

    Ref: code reformated, due to IDE settings issue

--- src/transition/Transition.java
+++ src/transition/Transition.java
@@ -6,12 +6,12 @@ import java.util.Objects;
 
 /**
  * <h2>Transition</h2>
- *
+ * <p>
  * Transition class is used
  * to represent a transition
  * between the states of a
  * formal computational machine.
- *
+ * <p>
  * Transition takes a tuple of
  * parameters, depending of which
  * type of machine we are simulating,
@@ -21,138 +21,138 @@ import java.util.Objects;
  * @version 1.0.0
  */
 public class Transition {
-	/**
-	 * Tuple of parameters of
-	 * the transition.
-	 */
-	private Tuple currentState;
+  /**
+   * Tuple of parameters of
+   * the transition.
+   */
+  private Tuple currentState;
 
-	/**
-	 * Tuple of the result of
-	 * the transition.
-	 */
-	private Tuple nextState;
+  /**
+   * Tuple of the result of
+   * the transition.
+   */
+  private Tuple nextState;
 
-	/**
-	 * Constructor of the class.
-	 *
-	 * It takes the tuple of the
-	 * parameters and the tuple of
-	 * the result.
-	 *
-	 * @param currentState current state of the transition.
-	 * @param nextState		 next state of the transition.
-	 * @throws NullPointerException if currentState or nextState
-	 * 															is {@code null}.
-	 */
-	public Transition(Tuple currentState, Tuple nextState) {
-		setCurrentState(currentState);
-		setNextState(nextState);
-	}
+  /**
+   * Constructor of the class.
+   * <p>
+   * It takes the tuple of the
+   * parameters and the tuple of
+   * the result.
+   *
+   * @param currentState current state of the transition.
+   * @param nextState    next state of the transition.
+   * @throws NullPointerException if currentState or nextState
+   *                              is {@code null}.
+   */
+  public Transition(Tuple currentState, Tuple nextState) {
+    setCurrentState(currentState);
+    setNextState(nextState);
+  }
 
-	/**
-	 * Gets the current state of the
-	 * transition.
-	 *
-	 * @return current state of the transition.
-	 */
-	public Tuple getCurrentState() {
-		return currentState;
-	}
+  /**
+   * Gets the current state of the
+   * transition.
+   *
+   * @return current state of the transition.
+   */
+  public Tuple getCurrentState() {
+    return currentState;
+  }
 
-	/**
-	 * Sets the current state of the
-	 * transition.
-	 *
-	 * @param currentState that we are going to set.
-	 * @throws NullPointerException if currentState is  {@code null}.
-	 */
-	public void setCurrentState(Tuple currentState) {
-		if (currentState == null)
-			throw new NullPointerException("current state can not be null.");
+  /**
+   * Sets the current state of the
+   * transition.
+   *
+   * @param currentState that we are going to set.
+   * @throws NullPointerException if currentState is  {@code null}.
+   */
+  public void setCurrentState(Tuple currentState) {
+    if (currentState == null)
+      throw new NullPointerException("current state can not be null.");
 
-		this.currentState = currentState;
-	}
+    this.currentState = currentState;
+  }
 
-	/**
-	 * Gets the next state of the transition.
-	 *
-	 * @return next state of the transition.
-	 */
-	public Tuple getNextState() {
-		return nextState;
-	}
+  /**
+   * Gets the next state of the transition.
+   *
+   * @return next state of the transition.
+   */
+  public Tuple getNextState() {
+    return nextState;
+  }
 
-	/**
-	 * Sets the next state of the
-	 * transition to the specified value.
-	 *
-	 * @param nextState of the transition.
-	 * @throws NullPointerException if nextState is  {@code null}.
-	 */
-	public void setNextState(Tuple nextState) {
-		if (nextState == null)
-			throw new NullPointerException("next state can not be null.");
+  /**
+   * Sets the next state of the
+   * transition to the specified value.
+   *
+   * @param nextState of the transition.
+   * @throws NullPointerException if nextState is  {@code null}.
+   */
+  public void setNextState(Tuple nextState) {
+    if (nextState == null)
+      throw new NullPointerException("next state can not be null.");
 
-		this.nextState = nextState;
-	}
+    this.nextState = nextState;
+  }
 
-	/**
-	 * Tests if two transitions
-	 * are equals.
-	 *
-	 * @param o object to be compared
-	 * @return {@code true} if transition
-	 * 					are equals.
-	 */
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Transition that = (Transition) o;
-		return Objects.equals(currentState, that.currentState) &&
-						Objects.equals(nextState, that.nextState);
-	}
+  /**
+   * Tests if two transitions
+   * are equals.
+   *
+   * @param o object to be compared
+   * @return {@code true} if transition
+   * are equals.
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Transition that = (Transition) o;
+    return Objects.equals(currentState, that.currentState) &&
+      Objects.equals(nextState, that.nextState);
+  }
 
-	/**
-	 * Hash code of the transition.
-	 *
-	 * @return hash code of the transition.
-	 */
-	@Override
-	public int hashCode() {
-		return Objects.hash(currentState, nextState);
-	}
+  /**
+   * Hash code of the transition.
+   *
+   * @return hash code of the transition.
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(currentState, nextState);
+  }
 
-	/**
-	 * Method that returns the transition
-	 * in a string representation, like
-	 * this:
-	 *
-	 * (i0, i1, ... , in) → (o0, 01, ... , 0m)
-	 *
-	 * @return transition in a string
-	 * 				 representation.
-	 */
-	@Override
-	public String toString() {
-		String transition = "(";
-		int index = 0;
-		for (Object elem : getCurrentState()) {
-			transition += index < getCurrentState().getSize() - 1 ?
-										elem.toString() + ", " :
-										elem.toString();
-			index += 1;
-		}
-		transition += ") → (";
-		index = 0;
-		for (Object elem : getNextState()) {
-			transition += index < getNextState().getSize() - 1 ?
-										elem.toString() + ", " :
-										elem.toString();
-			index += 1;
-		}
-		transition += ")";
-		return transition;
-	}
+  /**
+   * Method that returns the transition
+   * in a string representation, like
+   * this:
+   * <p>
+   * (i0, i1, ... , in) → (o0, 01, ... , 0m)
+   *
+   * @return transition in a string
+   * representation.
+   */
+  @Override
+  public String toString() {
+    String transition = "(";
+    int index = 0;
+    for (Object elem : getCurrentState()) {
+      transition += index < getCurrentState().getSize() - 1 ?
+        elem.toString() + ", " :
+        elem.toString();
+      index += 1;
+    }
+    transition += ") → (";
+    index = 0;
+    for (Object elem : getNextState()) {
+      transition += index < getNextState().getSize() - 1 ?
+        elem.toString() + ", " :
+        elem.toString();
+      index += 1;
+    }
+    transition += ")";
+    return transition;
+  }
 }
--- src/transition/TransitionTest.java
+++ src/transition/TransitionTest.java
@@ -12,40 +12,40 @@ import symbol.Symbol;
 
 public class TransitionTest {
 
-	Tuple currentState;
-	Tuple nextState;
+  Tuple currentState;
+  Tuple nextState;
 
-	@Before
-	public void creationOfTuples() {
-		currentState = new Triplet<State, Symbol, Symbol>(new State("q0"),
-																														new Symbol("a"),
-																														new Symbol("A"));
-		nextState = new Pair<State, Symbol>(new State("q1"),
-																							new Symbol("B"));
-	}
+  @Before
+  public void creationOfTuples() {
+    currentState = new Triplet<State, Symbol, Symbol>(new State("q0"),
+      new Symbol("a"),
+      new Symbol("A"));
+    nextState = new Pair<State, Symbol>(new State("q1"),
+      new Symbol("B"));
+  }
 
-	@Test
-	public void creationTest() {
-		Transition transition = new Transition(currentState, nextState);
-	}
+  @Test
+  public void creationTest() {
+    Transition transition = new Transition(currentState, nextState);
+  }
 
-	@Test(expected = NullPointerException.class)
-	public void currentStateNullCreationTest() {
-		new Transition(null, nextState);
-	}
+  @Test(expected = NullPointerException.class)
+  public void currentStateNullCreationTest() {
+    new Transition(null, nextState);
+  }
 
-	@Test(expected = NullPointerException.class)
-	public void nextStateNullCreationTest() {
-		new Transition(currentState, null);
-	}
+  @Test(expected = NullPointerException.class)
+  public void nextStateNullCreationTest() {
+    new Transition(currentState, null);
+  }
 
-	@Test
-	public void equalsTest() {
-		Transition transition1 = new Transition(currentState, nextState);
-		Transition transition2 = new Transition(currentState, nextState);
-		Transition transition3 = new Transition(nextState, currentState);
+  @Test
+  public void equalsTest() {
+    Transition transition1 = new Transition(currentState, nextState);
+    Transition transition2 = new Transition(currentState, nextState);
+    Transition transition3 = new Transition(nextState, currentState);
 
-		assertEquals(transition1, transition2);
-		assertNotEquals(transition1, transition3);
-	}
+    assertEquals(transition1, transition2);
+    assertNotEquals(transition1, transition3);
+  }
 }
